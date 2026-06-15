00e9537edd35baae2227752869b60be87da22941
dannamishelle23 <dannamishelle.23@gmail.com>
2026-06-15 00:54:55 -0500

    modificado database.js para deploy en render

--- backend/src/database.js
+++ backend/src/database.js
@@ -4,6 +4,7 @@ mongoose.set('strictQuery', true)
 
 const connection = async()=>{
     try {
+        console.log(process.env.MONGODB_URL)
         const {connection} = await mongoose.connect(process.env.MONGODB_URL)
         console.log(`Base de datos conectada con éxito`)
     } catch (error) {
